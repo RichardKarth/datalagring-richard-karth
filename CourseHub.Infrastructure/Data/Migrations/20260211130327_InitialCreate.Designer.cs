10f114418c15ed115380a8dc74fe76446b634f94
Richard Karth <richard.k.triumph@gmail.com>
2026-02-11 14:16:54 +0100

    added the tables into the database

--- CourseHub.Infrastructure/Data/Migrations/20260211130327_InitialCreate.Designer.cs
+++ CourseHub.Infrastructure/Data/Migrations/20260211130327_InitialCreate.Designer.cs
@@ -4,6 +4,7 @@ using CourseHub.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CourseHub.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(CHDbContext))]
-    partial class CHDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260211130327_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
