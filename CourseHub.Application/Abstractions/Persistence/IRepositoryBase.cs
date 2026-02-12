abd582d204e62037651caabf463a5f6d524c53cd
Richard Karth <richard.k.triumph@gmail.com>
2026-02-12 12:53:31 +0100

    Created a RepositoryBase

--- CourseHub.Application/Abstractions/Persistence/IRepositoryBase.cs
+++ CourseHub.Application/Abstractions/Persistence/IRepositoryBase.cs
@@ -1,19 +1,19 @@
 ﻿
 namespace CourseHub.Application.Abstractions.Persistence
 {
-    public interface IRepositoryBase<TModel, in TKey > where TModel : class where TKey : notnull
+    public interface IRepositoryBase<TModel, in TKey >
     {
 
 
         Task AddByIdAsync(TKey id, CancellationToken ct = default);
 
-        Task<TModel> GetByIdAsync(CancellationToken ct = default);
+        Task<TModel?> GetByIdAsync(TKey id, CancellationToken ct = default);
         Task<IReadOnlyList<TModel>> ListAsync(CancellationToken ct = default);
 
 
         Task UpdateAsync(TModel model, CancellationToken ct = default);
 
-        Task DeleteAsync(TModel model, CancellationToken ct = default);
+        Task DeleteByIdAsync(TKey id, CancellationToken ct = default);
 
     }
 }
---
+++ CourseHub.Infrastructure/Repositories/RepositoryBase.cs
@@ -0,0 +1,50 @@
+﻿using CourseHub.Application.Abstractions.Persistence;
+using CourseHub.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseHub.Infrastructure.Repositories;
+
+public abstract class RepositoryBase<TModel, TKey, TEntity>(CHDbContext context) : IRepositoryBase<TModel, TKey>
+    where TModel : class
+    where TEntity : class, 
+    IEntity<TKey>
+{
+
+    protected CHDbContext Context { get; } = context;
+    protected DbSet<TEntity> Set => Context.Set<TEntity>();
+
+
+
+
+    public abstract TModel ToModel(TEntity entity);
+    public abstract Task AddByIdAsync(TKey id, CancellationToken ct = default);
+    public abstract Task UpdateAsync(TModel model, CancellationToken ct = default);
+
+
+
+    public virtual async Task DeleteByIdAsync(TKey id, CancellationToken ct = default)
+    {
+        var entity = await Set.SingleOrDefaultAsync(x => x.Id!.Equals (id), ct);
+        if (entity == null) return;
+
+        Set.Remove(entity);
+    }
+
+    public virtual async Task<TModel?> GetByIdAsync(TKey id, CancellationToken ct = default)
+    {
+        var entity = await Set.AsNoTracking()
+            .SingleOrDefaultAsync(x => x.Id!.Equals(id), ct);
+
+        if (entity == null)
+            return default;
+
+        return ToModel(entity);
+    }
+
+    public virtual async Task<IReadOnlyList<TModel>> ListAsync(CancellationToken ct = default)
+    {
+        var entities = await Set.AsNoTracking().ToListAsync(ct);
+
+        return entities.Select(ToModel).ToList();
+    }
+}
