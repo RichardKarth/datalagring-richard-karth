b29ecde53e58c169fbb0287755c5be9d7891089f
Richard Karth <richard.k.triumph@gmail.com>
2026-02-15 15:46:29 +0100

    Interface for CourseRepository

---
+++ CourseHub.Application/Courses/PersistanceModels/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseHub.Application.Courses.PersistanceModels
+{
+    public class Course
+    {
+        public int Id { get; private set; }
+        public string Title { get; private set; } = null!;
+        public string? Description { get; private set; }
+        public int DurationDays { get; private set; }
+
+    }
+}
