0a2e31535aea475788c16902a54754c936aae47d
Richard Karth <richard.k.triumph@gmail.com>
2026-02-18 18:34:04 +0100

    Added Input and outputes for Enrollment

---
+++ CourseHub.Application/Enrollments/Outputs/EnrollmentStudentOutput.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseHub.Application.Enrollments.Outputs
+{
+    public record EnrollmentStudentOutput(int StudentId);
+}
